db5f5270d91f04a65fb80ec7116dbe51ad7e597d
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-07-06 20:12:03 +0900

    first Commit

---
+++ src/test/java/org/example/plandevelop/PlanDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+﻿package org.example.plandevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
