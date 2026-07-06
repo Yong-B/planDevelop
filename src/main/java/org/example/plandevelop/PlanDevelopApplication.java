198783fac97dfedc5cb471696136cbb1a6ec859a
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-07-06 20:08:15 +0900

    first Commit

---
+++ src/main/java/org/example/plandevelop/PlanDevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.plandevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanDevelopApplication.class, args);
+    }
+
+}
