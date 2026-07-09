1ea45e2acfa51e46e4ec746c427ca72c104dbc74
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-07-09 12:42:08 +0900

    Feat : Lv8 일정 페이징 조회 기능 구현

--- src/main/java/org/example/plandevelop/plan/domain/dto/PlanResponseDto.java
+++ src/main/java/org/example/plandevelop/plan/domain/dto/PlanResponseDto.java
@@ -11,14 +11,16 @@ public class PlanResponseDto {
     private final String title;
     private final String content;
     private String authorName;
+    private long commentCount;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
-    public PlanResponseDto(Plan plan) {
+    public PlanResponseDto(Plan plan, long commentCount) {
         this.id = plan.getId();
         this.title = plan.getTitle();
         this.content = plan.getContent();
         this.authorName = plan.getUser().getUsername();
+        this.commentCount = commentCount;
         this.createdAt = plan.getCreatedAt();
         this.modifiedAt = plan.getModifiedAt();
     }
